086deb48f9611395b852cf8a2d0e7e19fd04c76a
Torbjørn V Ness <torbness@gmail.com>
2026-09-17 09:43:07 +0200

    Fix setup file so that cell_models submodule is found

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup, find_packages
 from brainsignals import version
 
-
-setup(name='BrainSignals',
-      version=version.VERSION,
-      description='Package for making figures for Electric Brain Signals book',
-      author='LFPy-team',
-      author_email='lfpy@users.noreply.github.com',
-      packages=['brainsignals'],
-     )
+setup(
+    name='BrainSignals',
+    version=version.VERSION,
+    description='Package for making figures for Electric Brain Signals book',
+    author='LFPy-team',
+    author_email='lfpy@users.noreply.github.com',
+    packages=find_packages(),
+)
